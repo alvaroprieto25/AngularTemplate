4c99da65e3a8d63bbff4da7fb2c1583a72e56cdf
Alvaro Prieto <apm226@alu.ua.es>
2024-06-03 16:30:06 +0200

    Remove useless swiper config

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -32,15 +32,6 @@ export class HomeComponent implements AfterViewInit {
           disableOnInteraction: false,
         },
         slidesPerView: 1,
-        spaceBetween: 10,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
       });
     }
   }
