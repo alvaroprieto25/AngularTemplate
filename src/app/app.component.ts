14739403a1647a321a334492cdd4110fa56f8fa4
Alvaro Prieto <apm226@alu.ua.es>
2024-06-03 16:48:59 +0200

    Añadir icono al header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  isMobile: boolean;
 
+   constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe([
+      Breakpoints.Handset,
+      Breakpoints.Small,
+    ]).subscribe(result => {
+      console.log(result)
+      this.isMobile = result.matches;
+    });
+   }
 }
